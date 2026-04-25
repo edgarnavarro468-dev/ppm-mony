c1eb0dc3504717580dbf5e15324711fab1e223eb
edgarnavarro468-dev <edgarnavarro468@gmail.com>
2026-04-24 23:48:16 -0600

    primer commit

---
+++ database.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+engine = create_engine("sqlite:///ppm.db")
+
+Base = declarative_base()
+
+
+class Group(Base):
+    __tablename__ = "groups"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+
+class Expense(Base):
+    __tablename__ = "expenses"
+
+    id = Column(Integer, primary_key=True)
+    payer = Column(String)
+    amount = Column(Integer)
+    description = Column(String)
+
+
+Base.metadata.create_all(engine)
+
+print("Tablas creadas")
---
+++ testapp.py
@@ -0,0 +1,115 @@
+
+from database import Expense, Group
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database import Expense
+
+
+app = FastAPI()
+
+engine = create_engine("sqlite:///ppm.db")
+Session = sessionmaker(bind=engine)
+
+
+class GroupInput(BaseModel):
+    name: str
+
+
+
+class ExpenseInput(BaseModel):
+    payer: str
+    amount: int
+    description: str
+
+@app.get("/groups")
+def get_groups():
+
+    session = Session()
+
+    groups = session.query(Group).all()
+
+    result = []
+
+    for g in groups:
+        result.append({
+            "id": g.id,
+            "name": g.name
+        })
+
+    return result
+
+@app.post("/add-group")
+def add_group(group: GroupInput):
+
+    session = Session()
+
+    new_group = Group(
+        name=group.name
+    )
+
+    session.add(new_group)
+    session.commit()
+
+    return {"message":"Grupo creado"}
+
+@app.get("/")
+def root():
+    return {"message":"PPM funcionando"}
+
+
+@app.get("/expenses")
+def get_expenses():
+
+    session = Session()
+
+    expenses = session.query(Expense).all()
+
+    result = []
+
+    for e in expenses:
+        result.append({
+            "payer": e.payer,
+            "amount": e.amount,
+            "description": e.description
+        })
+
+    return result
+
+
+@app.post("/add-expense")
+def add_expense(expense: ExpenseInput):
+
+    session = Session()
+
+    new_expense = Expense(
+        payer=expense.payer,
+        amount=expense.amount,
+        description=expense.description
+    )
+
+    session.add(new_expense)
+    session.commit()
+
+    return {"message":"Gasto guardado"}
+
+
+@app.get("/balance/{total}/{people}")
+def balance(total:int, people:int):
+
+    share = total / people
+
+    return {
+        "payer":"Edgar",
+        "Diego_owes":share,
+        "Fernando_owes":share
+    }
+    
+    
+    
+    
+    
+    
